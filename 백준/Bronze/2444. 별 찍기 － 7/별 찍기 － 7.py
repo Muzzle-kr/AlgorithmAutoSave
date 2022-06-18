61519ebb3369e3b089495220a9a702e9395a08c1
한주형 <91880235+Jureamer@users.noreply.github.com>
2022-06-18 11:19:57 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 108 ms, Memory: 113248 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.py
@@ -0,0 +1,16 @@
+# 2 * (N - 1) 만큼의 칸이 존재해야함
+repeat = int(input())
+
+for i in range(1, repeat+1):
+  max = (2 * repeat - 1);
+  num = (2 * i - 1);
+  gap = int(((max - num) / 2))
+  # print("max = ", max, "num = ", num, "gap = ", gap);
+  print(" " * int((max - num)/2) + "*" * num)
+  
+  if i == repeat:
+    for j in range(repeat-1, 0, -1):
+      max = (2 * repeat - 1);
+      num = (2 * j - 1)
+      # print("max = ", max, "num = ", num, "gap = ", gap)
+      print(" " * int((max - num)/2) + "*" * num)
