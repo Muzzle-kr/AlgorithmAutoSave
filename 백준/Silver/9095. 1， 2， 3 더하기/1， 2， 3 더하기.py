fb5d8802f28e030d872bb00e51632f64a65b2c05
한주형 <91880235+Jureamer@users.noreply.github.com>
2022-07-31 11:29:08 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 116 ms, Memory: 113248 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -0,0 +1,9 @@
+repeat = int(input())
+arr = [0, 1, 2, 4]
+
+for i in range(4, 11):
+  arr.append(arr[i - 3] + arr[i - 2] + arr[i - 1])
+
+for _ in range(repeat):
+  n = int(input())
+  print(arr[n])
