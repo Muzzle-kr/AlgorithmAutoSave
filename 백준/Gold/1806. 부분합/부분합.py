1795ed5c378d54dcc42ab7c58240bd8cf7600ad8
한주형 <91880235+Jureamer@users.noreply.github.com>
2022-07-31 17:17:05 +0900

    [Gold IV] Title: 부분합, Time: 156 ms, Memory: 128152 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.py
+++ 백준/Gold/1806. 부분합/부분합.py
@@ -1,9 +1,3 @@
-# 8 2
-# 1 2 3 4 5 6 7 8
-
-# 가장 긴 짝수 연속한 부분의 수열을 구하라
-# 총 M개 삭제가 가능하기에 총 길이의 arr 
-# 슬라이딩 윈도우로 구하기
 import sys
 
 input = sys.stdin.readline
@@ -19,7 +13,6 @@ lengthArr = []
 total = arr[lt]
 
 while rt < N or lt < rt:
-  # print(f'lt = {lt}, rt = {rt}')
   try:
     if total >= M:
       lengthArr.append(rt - lt + 1)
@@ -32,7 +25,6 @@ while rt < N or lt < rt:
     break
 
 if len(lengthArr) > 0:
-  # print(lengthArr)
   print(min(lengthArr))
 else:
   print(0)
