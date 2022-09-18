cc93be8229dc3d189b45e2b00adfa349b6ff7753
한주형 <91880235+Jureamer@users.noreply.github.com>
2022-09-18 23:17:21 +0900

    [Silver IV] Title: 보물, Time: 108 ms, Memory: 113248 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.py
@@ -0,0 +1,13 @@
+repeat = int(input())
+
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+a.sort()
+b.sort(reverse=True)
+sum = 0
+
+for i in range(repeat):
+  sum += (a[i] * b[i])
+
+print(sum)
