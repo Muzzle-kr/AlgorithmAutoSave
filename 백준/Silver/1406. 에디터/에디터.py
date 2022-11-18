134c03b006d5c12244fe4d4930685fb8ea9c79a6
한주형 <91880235+Jureamer@users.noreply.github.com>
2022-11-18 16:07:54 +0900

    [Silver II] Title: 에디터, Time: 284 ms, Memory: 173420 KB -BaekjoonHub

---
+++ 백준/Silver/1406. 에디터/에디터.py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin.readline
+
+str1 = list(input().rstrip())
+str2 = []
+
+for _ in range(int(input())):
+    order = list(input().rstrip())
+    
+    if order[0] == "L":
+        if str1:
+            str2.append(str1.pop())
+            
+    elif order[0] == "D":
+        if str2:
+            str1.append(str2.pop())
+            
+    elif order[0] == "B":
+        if str1:
+            str1.pop()
+            
+    else:
+        str1.append(order[2])
+
+str1.extend(reversed(str2))
+print(''.join(str1))
