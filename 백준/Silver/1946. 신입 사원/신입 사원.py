4f346e37e575aeae3278a0b30891507fd3ae7789
한주형 <91880235+Jureamer@users.noreply.github.com>
2022-11-24 15:34:26 +0900

    [Silver I] Title: 신입 사원, Time: 3740 ms, Memory: 221128 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.py
+++ 백준/Silver/1946. 신입 사원/신입 사원.py
@@ -1,18 +1,32 @@
 import sys
+import heapq as hq
 input = sys.stdin.readline
 
-T = int(input())
+N = int(input())
 
-for _ in range(T):
-    N = int(input())
-    rank = [list(map(int, input().split())) for _ in range(N)]
-    rank_asc = sorted(rank)
-    top = 0
-    result = 1
+for _ in range(N):
+    M = int(input())
     
-    for i in range(1, len(rank_asc)):
-        if rank_asc[i][1] < rank_asc[top][1]:
-            top = i
-            result += 1
+    pool = []
+    for i in range(M):
+        pool.append(list(map(int, input().split())))
+    # 첫번째 성적순 정리
     
-    print(result)
+    pool = sorted(pool, key=lambda x: x[0]) 
+    
+    dropout = 1
+    order = 0
+    lastDropout = 0
+    for i in range(1, M):
+        currentApplicant = pool[i]
+        comparison = pool[lastDropout]
+        # print(f'i: {i}, currentApplicant: {currentApplicant}, comparison: {comparison}')
+        # 면접 심사 순위가 더 떨어지는 사람을 탈락시킴
+        if pool[i][1] < pool[lastDropout][1]:
+            # print(f'살아남은자: {i}')
+            # 탈락자 추가
+            dropout += 1
+            # 마지막 탈락자 index 변경
+            lastDropout = i
+        
+    print(dropout)
