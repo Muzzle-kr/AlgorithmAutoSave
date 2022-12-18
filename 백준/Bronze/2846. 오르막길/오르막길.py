c283b23475a1f82a0892b9aa3d3f6fbaf9cc78e5
한주형 <91880235+Jureamer@users.noreply.github.com>
2022-12-18 10:50:22 +0900

    [Bronze I] Title: 오르막길, Time: 108 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Bronze/2846. 오르막길/오르막길.py
+++ 백준/Bronze/2846. 오르막길/오르막길.py
@@ -1,4 +1,3 @@
-
 result = [0]
 
 N = int(input())
@@ -7,22 +6,17 @@ roads = list(map(int, input().split()))
 heights = 0
 tmp = []
 
-# print(N, roads)
-
-for idx, i in enumerate(roads):
-    # print(f'i : {i}, tmp: {tmp}')
+for i in roads:
     if len(tmp) == 0:
         tmp.append(i)
     else:
         # 오르막길이라면
         if tmp[-1] < i:
-            # print("오르막길입니다.")
             heights += (i - tmp[-1])
             result.append(heights)
             tmp.append(i)  
         # 내리막길 혹은 평지
         else:
-            # print("내리막길 혹은 평지입니다.")
             heights = 0
             tmp = [i]
 
