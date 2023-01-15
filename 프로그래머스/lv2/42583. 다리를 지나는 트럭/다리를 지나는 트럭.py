7133627391ff42519e043d7a1d8ecc24f1b55f06
한주형 <91880235+Jureamer@users.noreply.github.com>
2023-01-15 17:23:32 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
+++ 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -6,10 +6,14 @@ def solution(bridge_length, weight, truck_weights):
     bridge_weight = 0
     answer = 0
     length = 0    
+    # 트럭 대기 줄이 끝날 때까지 반복 
+    # bridge의 마지막에 트럭이 있을 경우 트럭을 빼주고 rotate 해준다.
+    # 트럭 무게 합이 다리 무게 이하일 경우 트럭을 집어넣고 rotate 해준다.
+    # rotate
+    
+    
     
     while truck_weights:
-        answer += 1
-        bridge.rotate(-1)    
         if bridge_weight + truck_weights[0] <= weight and length < bridge_length:
             truck = truck_weights.pop(0)
             bridge[-1] = truck
@@ -20,10 +24,15 @@ def solution(bridge_length, weight, truck_weights):
             bridge_weight -= bridge[0]
             bridge[0] = 0
             length -= 1
+        answer += 1
+        # print(bridge, truck_weights, answer)
+        
+            
+        bridge.rotate(-1)    
     
     for i in range(bridge_length - 1, 0, -1):
         if bridge[i] != 0:
             answer += i + 1
             break
         
-    return answer
+    return answer + 1
