b1ec814ac8d985fb4f4bd1005f4a3538b338e926
한주형 <91880235+Jureamer@users.noreply.github.com>
2023-01-16 12:49:00 +0900

    [level 3] Title: 입국심사, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv3/43238. 입국심사/입국심사.py
+++ 프로그래머스/lv3/43238. 입국심사/입국심사.py
@@ -1,22 +1,21 @@
 def solution(n, times):
     answer = 0
-    # 사용 중이면 1, 사용 중이 아니면 0
     min_time = times[0]
     max_time = times[0] * n
-    
+
     while True:
         mid_time = (min_time + max_time) // 2
         count = 0
         
-        for time in times:
-            count += mid_time // time
+        for t in times:
+            count += mid_time // t
         
-        if count >= n:
-            answer = mid_time
-            max_time = mid_time - 1
-        else:
+        if count < n:
             min_time = mid_time + 1
-            
+        else:
+            max_time = mid_time - 1
+        
         if min_time > max_time:
+            answer = min_time
             break
-    return answer 
+    return answer
