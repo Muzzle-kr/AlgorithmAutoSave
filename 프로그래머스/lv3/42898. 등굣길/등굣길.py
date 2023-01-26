1d875ab3bc011a214b8091dd7dbb78e406695079
한주형 <91880235+Jureamer@users.noreply.github.com>
2023-01-26 12:53:27 +0900

    [level 3] Title: 등굣길, Time: 2.64 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv3/42898. 등굣길/등굣길.py
+++ 프로그래머스/lv3/42898. 등굣길/등굣길.py
@@ -1,24 +1,3 @@
 def solution(m, n, puddles):
-    dp = [[0] * (101) for _ in range(101)]
-
-    for x, y in puddles:
-        dp[y][x] = -1
-    
-    dp[1][1] = 1
-        
-    for i in range(1, n+1):
-        for j in range(1, m+1):
-            if dp[i][j] == -1:
-                continue
-            
-            a = 0
-            b = 0
-            
-            if dp[i-1][j] != -1:
-                a = dp[i-1][j]
-            
-            if dp[i][j-1] != -1:
-                b = dp[i][j-1]
-            
-            dp[i][j] += (a + b) % 1000000007
-    return dp[n][m]
+    answer = 0
+    return answer
