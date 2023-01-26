5d3dc4fcf2ea17ad08d9c8f4077ab32a0f635154
한주형 <91880235+Jureamer@users.noreply.github.com>
2023-01-26 11:30:42 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -0,0 +1,10 @@
+
+
+# 보호 시작일보다 입양일이 더 빠른 동물
+# 아이디와 이름 조회
+# 보호 시작일이 빠른 순
+SELECT i.ANIMAL_ID, i.NAME
+FROM ANIMAL_INS i JOIN ANIMAL_OUTS o
+ON i.ANIMAL_ID = o.ANIMAL_ID
+WHERE i.DATETIME > o.DATETIME
+ORDER BY i.DATETIME
