565d303aa5030a67e06c18431d93c7ea0c92f124
한주형 <91880235+Jureamer@users.noreply.github.com>
2023-02-08 10:43:28 +0900

    [Silver III] Title: N과 M (3), Time: 932 ms, Memory: 158040 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+n, m = map(int, input().split())
+
+isUsed = [False] * (n + 1)
+
+def func(arr):
+    if len(arr) >= m:
+        print(*arr)
+        return
+
+    for i in range(1, n + 1):
+        arr.append(i)
+        func(arr)
+        arr.pop()
+        
+for i in range(1, n + 1):
+    arr = []
+    arr.append(i)
+    func(arr)
