d30a7a6cd57d6ed0abb28b02fa9cb9539db5a78c
한주형 <91880235+Jureamer@users.noreply.github.com>
2023-02-08 13:02:04 +0900

    [Silver II] Title: N과 M (12), Time: 212 ms, Memory: 119428 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.py
@@ -0,0 +1,30 @@
+import sys
+input = sys.stdin.readline
+n, m = map(int, input().split())
+isUsed = [False] * (100001)
+initialArr = sorted(list(map(int, input().split())))
+checkDuplicated = {}
+
+def func(arr):
+    global checkDuplicated
+    if len(arr) == m:
+        if str(arr) in checkDuplicated:
+            return
+        
+        checkDuplicated[str(arr)] = True
+        print(*arr)
+        return
+
+    for new_idx in range(n):
+        if isUsed[new_idx] or arr[-1] > initialArr[new_idx]:
+            continue
+        
+        arr.append(initialArr[new_idx])
+        func(arr)
+        arr.pop()
+        
+new_arr = []
+for idx in range(len(initialArr)):
+    new_arr.append(initialArr[idx])
+    func(new_arr)
+    new_arr.pop()
