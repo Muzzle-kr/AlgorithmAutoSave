94c4bb03e2f33a09327fe8398d2bbc3de1b3dd59
한주형 <91880235+Jureamer@users.noreply.github.com>
2023-02-12 21:48:09 +0900

    [Gold V] Title: 멀티버스 Ⅱ, Time: 628 ms, Memory: 171512 KB -BaekjoonHub

--- 백준/Gold/18869. 멀티버스 Ⅱ/멀티버스 Ⅱ.py
+++ 백준/Gold/18869. 멀티버스 Ⅱ/멀티버스 Ⅱ.py
@@ -1,19 +1,20 @@
-import sys
-input = sys.stdin.readline
 from collections import defaultdict
-
-m, n = map(int, input().split())
+n, m = map(int, input().split())
 universe = defaultdict(int)
 
-for _ in range(m):
-    arr = list(map(int, input().split()))
-    sorted_arr = sorted(list(set(arr)))
-    dict_arr = {sorted_arr[i]: i for i in range(len(sorted_arr))}
-    c = tuple([dict_arr[i] for i in arr])
+for i in range(n):
+    planet = list(map(int, input().split()))
+    sorted_planet = sorted(list(set(planet)))
+    # index_arr = tuple([sorted_planet.index(i) for i in planet])
+    
+    dict_arr = {sorted_planet[i]: i for i in range(len(sorted_planet))}
+    c = tuple([dict_arr[i] for i in planet])
+    # print(c)
     universe[c] += 1
-
+    # print(universe)
 sum = 0
+
 for i in universe.values():
-    sum += i * (i - 1) // 2
+    sum += (i * (i - 1)) // 2
 
 print(sum)
