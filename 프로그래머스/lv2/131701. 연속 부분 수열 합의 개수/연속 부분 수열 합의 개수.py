6ce037fbd823136e4d791de0182d4721e6490a68
한주형 <91880235+Jureamer@users.noreply.github.com>
2023-02-15 21:33:10 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 4701.79 ms, Memory: 43.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -0,0 +1,17 @@
+def solution(elements):
+    answer = set()
+    length = len(elements)
+    
+    for i in elements:
+        answer.add(i)
+        
+    totalSum = sum(elements)
+    answer.add(totalSum)
+
+    elements = elements * 2
+    
+    for limitLength in range(2, length):
+        for startIdx in range(length):
+            answer.add(sum(elements[startIdx:startIdx+limitLength]))
+
+    return len(answer)
