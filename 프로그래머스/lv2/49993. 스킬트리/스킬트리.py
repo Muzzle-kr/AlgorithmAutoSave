e9b39d45931a9186dfaddc78aef8f4c550f1536e
한주형 <91880235+Jureamer@users.noreply.github.com>
2023-02-17 09:50:34 +0900

    [level 2] Title: 스킬트리, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/49993. 스킬트리/스킬트리.py
@@ -0,0 +1,15 @@
+def solution(skill, skill_trees):
+    answer = 0
+    
+    for user_skill in skill_trees:
+        remainSkill = "".join([s for s in user_skill if s in skill])
+        isSame = True
+        
+        for i in range(len(remainSkill)):
+            if remainSkill[i] != skill[i]:
+                isSame = False
+                break
+        if isSame:
+            answer += 1
+
+    return answer
