7dfd16610a628206916de5268fe4ef6b520c620b
한주형 <91880235+Jureamer@users.noreply.github.com>
2023-02-18 10:52:52 +0900

    [unrated] Title: 뒤에 있는 큰 수 찾기, Time: 271.76 ms, Memory: 116 MB -BaekjoonHub

--- 프로그래머스/unrated/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.py
+++ 프로그래머스/unrated/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.py
@@ -1,22 +1,25 @@
 def solution(numbers):
-    answer = [0] * len(numbers)
+    answer = []
     
-    indexStack = []
-    valueStack = []
+    bigNumber = []
     
-    for idx, val in enumerate(numbers):
-        if idx == 0:
-            indexStack.append(idx)
-            valueStack.append(val)
-        else:
-            if val > valueStack[-1]:
-                while valueStack and val > valueStack[-1]:
-                    answer[indexStack.pop()] = val
-                    valueStack.pop()
-            indexStack.append(idx)
-            valueStack.append(val)
-    
-    while indexStack:
-        answer[indexStack.pop()] = -1
+    for i in range(len(numbers)-1, -1, -1):
+        if i == len(numbers)-1:
+            bigNumber.append(numbers[i])
+            answer.append(-1)
+        else: 
+            count = 1
+            isFind = False
+            while count < len(bigNumber) + 1:
+                # print(f'bigNumber: {bigNumber}, count: {count}, i: {i}, numbers[i]: {numbers[i]}')
+                if numbers[i] < bigNumber[-count]:
+                    answer.append(bigNumber[-count])
+                    bigNumber.append(numbers[i])
+                    isFind = True
+                    break
+                count += 1
+            if not isFind:
+                bigNumber.append(numbers[i])
+                answer.append(-1)
 
-    return answer
+    return answer[::-1]
