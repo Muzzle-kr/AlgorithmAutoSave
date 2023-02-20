eedaf53ad85a3eec303e8d89d376a4e2e1be361f
한주형 <91880235+Jureamer@users.noreply.github.com>
2023-02-20 10:47:43 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 79.17 ms, Memory: 12.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.py
@@ -0,0 +1,27 @@
+def conquer(arr):
+    length = len(arr)
+    # 더 이상 분할할 수 없을 떄
+    if length < 2:
+        answer[arr[0][0]] += 1
+        return
+    
+    # 분할할 수 있을 때
+    previous = arr[0][0]
+    for i in range(length):
+        for j in range(length):
+            if arr[i][j] != previous:
+                conquer([row[:length//2] for row in arr[:length//2]])
+                conquer([row[length//2:] for row in arr[:length//2]])
+                conquer([row[:length//2] for row in arr[length//2:]])
+                conquer([row[length//2:] for row in arr[length//2:]])
+                return
+            
+    answer[previous] += 1
+    
+def solution(arr):
+    global answer
+    answer = [0, 0]
+    
+
+    conquer(arr)
+    return answer
