cac522c773029e8a9f3df25d5c9814305e566516
한주형 <91880235+Jureamer@users.noreply.github.com>
2023-02-20 16:13:50 +0900

    [level 2] Title: 괄호 변환, Time: 0.12 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/60058. 괄호 변환/괄호 변환.py
@@ -0,0 +1,63 @@
+def divide(p):
+    left = 0
+    right = 0
+    
+    for i in p:
+        if i == "(":
+            left += 1
+        else:
+            right += 1
+        
+        if left == right:
+            return p[:left+right], p[left+right:]
+
+def check(u):
+    stack = []
+    
+    for i in u:
+        if i == "(":
+            stack.append(i)
+        else:
+            if stack:
+                stack.pop()
+            else:
+                return False
+    if not stack:
+        return True 
+    else:
+        return False
+
+def solution(p):
+    
+    # 1단계
+    if not p:
+        return ""
+    
+    
+    # 2단계
+    u, v = divide(p)
+    
+    # 3단계
+    if check(u):
+        # 3-1단계
+        u += solution(v)
+        return u
+    # 4단계
+    else:
+        # 4-1단계
+        answer = "("
+        # 4-2 단계
+        answer += solution(v)
+        # 4-3 단계
+        answer += ")"
+        
+        # 4-4단계
+        u = list(u[1:-1])
+        for i in range(len(u)):
+            if u[i] == "(":
+                u[i] = ")"
+            else:
+                u[i] = "("
+        answer += "".join(u)
+        
+        return answer
