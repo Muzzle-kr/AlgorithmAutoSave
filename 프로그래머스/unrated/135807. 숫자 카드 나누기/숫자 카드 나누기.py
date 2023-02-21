15a927b255dadff0e7107b64ee5bd89671bf64b8
한주형 <91880235+Jureamer@users.noreply.github.com>
2023-02-21 12:15:53 +0900

    [unrated] Title: 숫자 카드 나누기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/135807. 숫자 카드 나누기/숫자 카드 나누기.py
+++ 프로그래머스/unrated/135807. 숫자 카드 나누기/숫자 카드 나누기.py
@@ -4,30 +4,39 @@ def gcd(a, b):
     return gcd(b, a % b)
 
 def solution(arrayA, arrayB):
+    answer = 0
+    
     tmp = []
     for a in arrayA:
         if not tmp:
             tmp.append(a)
             continue
         else:
-            tmp.append(gcd(a, tmp[-1]))
+            gcdd = gcd(tmp[-1], a)
+            tmp.append(gcdd)
             
     tmp2 = []
+
     for b in arrayB:
         if not tmp2:
             tmp2.append(b)
             continue
         else:
-            tmp2.append(gcd(b, tmp2[-1]))
+            gcdd = gcd(tmp2[-1], b)
+            tmp2.append(gcdd)
 
     gcd1 = tmp[-1]
     gcd2 = tmp2[-1]
 
+    
+    # print(gcd1, gcd2)
     for i in range(len(arrayA)):
-        if gcd2 >= 1 and arrayA[i] % gcd2 == 0:
+        if gcd2 != 1 and arrayA[i] % gcd2 == 0:
             gcd2 = 0
+            return 0
         
-        if gcd1 >= 1 and arrayB[i] % gcd1 == 0:
+        if gcd1 != 1 and arrayB[i] % gcd1 == 0:
             gcd1 = 0
+            return 0
         
     return max(gcd1, gcd2)
