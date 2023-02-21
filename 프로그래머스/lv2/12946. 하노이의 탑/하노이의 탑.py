c8b83ff37c16df8be01b48c86cb9e453dde4fd01
한주형 <91880235+Jureamer@users.noreply.github.com>
2023-02-21 12:29:14 +0900

    [level 2] Title: 하노이의 탑, Time: 8.55 ms, Memory: 18.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12946. 하노이의 탑/하노이의 탑.py
@@ -0,0 +1,16 @@
+def hanoi(n, start, end, assist):
+    global answer
+    if n == 1:
+        answer.append([start, end])
+        return
+    
+    hanoi(n-1, start, assist, end)
+    answer.append([start, end])
+    hanoi(n-1, assist, end, start)
+    
+def solution(n):
+    global answer
+    answer = []
+    
+    hanoi(n, 1, 3, 2)
+    return answer
