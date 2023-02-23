f2f8ff2dd3ade267d2dec1fe1ea188eb4c20661f
한주형 <91880235+Jureamer@users.noreply.github.com>
2023-02-24 08:40:36 +0900

    [unrated] Title: 대충 만든 자판, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/160586. 대충 만든 자판/대충 만든 자판.py
@@ -0,0 +1,22 @@
+def solution(keymap, targets):
+    answer = []
+    
+    for i in range(len(targets)):
+        sum = 0
+        for j in range(len(targets[i])):
+            findTarget = targets[i][j]
+            
+            minIdx = int(1001)
+            for k in range(len(keymap)):
+                a = 1001 if keymap[k].find(findTarget) == -1 else keymap[k].find(findTarget)
+                minIdx = min(minIdx, a)
+                
+            # print(f'k: {k}, minIdx: {minIdx}, findTarget: {findTarget}, keymap[k]: {keymap[k]}')                
+            if minIdx != 1001:
+                sum += minIdx + 1
+        
+        if sum != 0:
+            answer.append(sum)
+        else:
+            answer.append(-1)                
+    return answer
