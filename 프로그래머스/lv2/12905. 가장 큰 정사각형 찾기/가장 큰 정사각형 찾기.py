c369473735899074917850b94d9ed48d0cfb8fa3
한주형 <91880235+Jureamer@users.noreply.github.com>
2023-02-24 10:17:50 +0900

    [level 2] Title: 가장 큰 정사각형 찾기, Time: 527.74 ms, Memory: 38.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.py
+++ 프로그래머스/lv2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.py
@@ -1,23 +1,22 @@
 def solution(board):
-    answer = 1234
-    n = len(board)
-    m = len(board[0])
+    answer = -1
+    dp = [[0] * len(board[0]) for _ in range(len(board))]
+    dp[0] = board[0]
     
-    dp = [[0] * m for _ in range(n)]
-    
-    for i in range(n):
+    # for b in board:
+    #     print(b)
+    for i in range(len(board)):
         dp[i][0] = board[i][0]
-    
-    for j in range(m):
-        dp[0][j] = board[0][j]
         
-    for i in range(1, n):
-        for j in range(1, m):
+    for i in range(1, len(board)):
+        for j in range(1, len(board[0])):
             if board[i][j] == 1:
-                dp[i][j] = min(dp[i-1][j-1], dp[i-1][j], dp[i][j-1]) + 1
-
-    answer = 0
-    for i in range(n):
-        answer = max(answer, max(dp[i]))
+                dp[i][j] = min(dp[i-1][j], dp[i-1][j-1], dp[i][j-1]) + 1
     
+    # for d in dp:
+    #     print(d)
+    for i in range(1, len(board)):
+        for j in range(1, len(board[0])):
+            answer = max(answer, dp[i][j])
+
     return answer ** 2
