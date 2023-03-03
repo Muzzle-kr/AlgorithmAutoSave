bae40067c3b124590e29865289c80d9e7bfd8cbc
한주형 <91880235+Jureamer@users.noreply.github.com>
2023-03-03 22:47:32 +0900

    [level 4] Title: 식품분류별 가장 비싼 식품의 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv4/131116. 식품분류별 가장 비싼 식품의 정보 조회하기/식품분류별 가장 비싼 식품의 정보 조회하기.sql
+++ 프로그래머스/lv4/131116. 식품분류별 가장 비싼 식품의 정보 조회하기/식품분류별 가장 비싼 식품의 정보 조회하기.sql
@@ -6,39 +6,29 @@
 # GROUP BY 1
 # ORDER BY 2 DESC
 
-
-# # SELECT * FROM FOOD_PRODUCT
-# # WHERE CATEGORY IN ('과자','국','김치','식용유')
-# # ORDER BY CATEGORY, PRICE DESC
-
-# # 과자 1950, 국 2900, 식용유 8950
-
-
-
-
-
-
-
-
-
-
-
-
-
-SELECT 
-    CATEGORY, 
-    PRICE,
-    PRODUCT_NAME
-FROM 
-    FOOD_PRODUCT
-WHERE 
-    CATEGORY REGEXP '과자|국|김치|식용유'
-    AND PRICE IN (SELECT MAX(PRICE) p
-    FROM FOOD_PRODUCT
-    GROUP BY CATEGORY) 
-GROUP BY 1
+SELECT CATEGORY, MAX_PRICE, PRODUCT_NAME
+FROM (
+        SELECT CATEGORY, PRICE AS MAX_PRICE, PRODUCT_NAME, rank() over(partition by category order by price desc) r # 그룹별 랭킹
+        FROM FOOD_PRODUCT
+    ) tmp
+WHERE CATEGORY REGEXP '과자|국|김치|식용유'
+AND r = 1
 ORDER BY 2 DESC
 
+# 과자 1950, 국 2900, 식용유 8950
+
+# SELECT 
+#     CATEGORY, 
+#     PRICE,
+#     PRODUCT_NAME
+# FROM 
+#     FOOD_PRODUCT
+# WHERE 
+#     CATEGORY REGEXP '과자|국|김치|식용유'
+# AND 
+#     PRICE IN (SELECT MAX(PRICE) FROM FOOD_PRODUCT GROUP BY CATEGORY)
+# GROUP BY 1
+# ORDER BY 2 DESC
 
 
 
@@ -49,3 +39,14 @@ ORDER BY 2 DESC
 
 
 
+
+
+
+
+
+
+
+# 김치	19000	맛있는배추김치
+# 식용유	8950	맛있는콩기름
+# 국	2900	맛있는미역국
+# 과자	1950	맛있는포카칩
