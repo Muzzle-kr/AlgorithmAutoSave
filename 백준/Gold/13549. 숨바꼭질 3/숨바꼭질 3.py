b4db98065b41542df3fe3d9f33990e200db999f8
한주형 <91880235+Jureamer@users.noreply.github.com>
2023-03-31 09:43:59 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 216 ms, Memory: 124940 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -0,0 +1,29 @@
+from collections import deque
+from collections import defaultdict
+n, m = map(int, input().split())
+case_number = 100001
+visit = [False] * 100001
+
+def bfs():
+    
+    dq = deque()
+    dq.append((n, 0))
+    
+    while dq:
+        x, count = dq.popleft()
+        visit[x] = True
+        
+        if x == m:
+            return count
+        
+        if 0 <= x*2 <= 100000 and not visit[x*2]:
+            dq.append((x*2, count))
+            
+        if 0 <= x-1 <= 100000 and not visit[x-1]:
+            dq.append((x-1, count+1))
+        
+        if 0 <= x+1 <= 100000 and not visit[x+1]:
+            dq.append((x+1, count+1))
+            
+            
+print(bfs())
