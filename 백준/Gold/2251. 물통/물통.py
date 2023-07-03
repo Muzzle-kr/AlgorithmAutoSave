9a6911e1dbea7290d71212c799998368c82f79d4
한주형 <91880235+Jureamer@users.noreply.github.com>
2023-07-03 09:14:47 +0900

    [Gold V] Title: 물통, Time: 148 ms, Memory: 115596 KB -BaekjoonHub

---
+++ 백준/Gold/2251. 물통/물통.py
@@ -0,0 +1,55 @@
+from collections import deque
+import heapq as hq
+
+def pour(x, y):
+    if not visited[x][y]:
+        visited[x][y] = True
+        q.append((x, y))
+        
+def bfs():    
+    while q:
+        x, y = q.popleft()
+        
+        z = c - x - y
+        if x == 0:
+            hq.heappush(result, z)
+            
+        # x -> y
+        water = min(x, b-y)
+        pour(x - water, y + water)
+        
+        # x -> z
+        water = min(x, c-z)
+        pour(x - water, y)
+        
+        # y -> x
+        water = min(y, a-x)
+        pour(x + water, y - water)
+        
+        # y -> z
+        water = min(y, c-z)
+        pour(x, y - water)
+        
+        # z -> x
+        water = min(z, a-x)
+        pour(x + water, y)
+        
+        # z -> y
+        water = min(z, b-y)
+        pour(x, y + water)
+        
+            
+            
+a, b, c = map(int, input().split())
+q = deque()
+q.append((0, 0))   
+
+visited = [[False] * (b+1) for _ in range(a+1)]
+visited[0][0] = True
+
+result = []
+bfs()
+
+result.sort()
+
+print(*result)
