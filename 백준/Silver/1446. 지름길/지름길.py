df47875dfa145f2bbea723d9fb190036faa22433
한주형 <91880235+Jureamer@users.noreply.github.com>
2023-07-20 13:39:10 +0900

    [Silver I] Title: 지름길, Time: 152 ms, Memory: 115180 KB -BaekjoonHub

---
+++ 백준/Silver/1446. 지름길/지름길.py
@@ -0,0 +1,39 @@
+import heapq as hq
+import sys
+
+def dijkstra(start):
+    q = []
+    hq.heappush(q,(0,start))
+    distance[start] = 0
+    while q:
+        dist, now = hq.heappop(q)
+
+        #지금 힙큐에서 뺀게 now까지 가는데 최소비용이 아닐수도 있으니 체크
+        if dist > distance[now]:
+            continue
+
+        for i in graph[now]:
+            cost = dist + i[1]
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                hq.heappush(q,(cost, i[0]))
+
+
+INF = int(1e9)
+n , d = map(int,input().split())
+graph = [[] for _ in range(d+1)]
+distance = [INF] * (d+1)
+
+#일단 거리 1로 초기화.
+for i in range(d):
+    graph[i].append((i+1, 1))
+
+#지름길 있는 경우에 업데이트
+for _ in range(n):
+    s, e, l = map(int,input().split())
+    if e > d:# 고려 안해도 됨.
+        continue
+    graph[s].append((e, l))
+
+dijkstra(0)
+print(distance[d])
