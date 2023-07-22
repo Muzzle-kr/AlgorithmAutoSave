910961f6a8687a30e1db3ca83f97ca573d9f9a96
한주형 <91880235+Jureamer@users.noreply.github.com>
2023-07-22 10:24:20 +0900

    [Gold III] Title: 최소비용 구하기 2, Time: 360 ms, Memory: 129500 KB -BaekjoonHub

--- 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.py
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.py
@@ -1,6 +1,30 @@
 import heapq as hq
+
+def dijkstra(start):
+    q = []
+    distance = [float('inf')] * (n+1)
+    nearest = [float('inf')] * (n+1)
+    distance[start] = 0
+    hq.heappush(q, (0, start))
+    
+    while q:
+        c, now = hq.heappop(q)
+        
+        if c > distance[now]:
+            continue
+        
+        for next_node, next_cost in graph[now]:
+            cost = c + next_cost
+            
+            if cost < distance[next_node]:
+                distance[next_node] = cost
+                nearest[next_node] = now
+                q.append((cost, next_node))
+    return distance, nearest
+    
 n = int(input())
 m = int(input())
+
 graph = [[] for _ in range(n+1)]
 
 for _ in range(m):
@@ -9,34 +33,18 @@ for _ in range(m):
 
 start, end = map(int, input().split())
 
-nearest = [float('inf')] * (n+1)
-distance = [float('inf')] * (n+1)
+dist, nearest = dijkstra(start)
 
-q = [(0, start)]
-
-while q:
-    dist, now = hq.heappop(q)
-    
-    if dist > distance[now]:
-        continue
-    
-    for next, next_dist in graph[now]:
-        cost = dist + next_dist
-        
-        if cost < distance[next]:
-            distance[next] = cost
-            nearest[next] = now
-            hq.heappush(q, (cost, next))
 ans = []
 node = end
 
 while node != start:
     ans.append(node)
     node = nearest[node]
-    
-ans.append(str(start))
+
+ans.append(start)
 ans.reverse()
 
-print(distance[end])
+print(dist[end])
 print(len(ans))
 print(*ans)
