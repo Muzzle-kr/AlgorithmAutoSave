60b3d89d3668fe94a4b772175bd59584c983ec89
한주형 <91880235+Jureamer@users.noreply.github.com>
2023-07-24 14:19:56 +0900

    [Gold III] Title: 웜홀, Time: 352 ms, Memory: 118052 KB -BaekjoonHub

--- 백준/Gold/1865. 웜홀/웜홀.py
+++ 백준/Gold/1865. 웜홀/웜홀.py
@@ -1,44 +1,33 @@
-for _ in range(int(input())):
-    
-    def bf():
-        distance = [INF] * (N+1)
-        for i in range(N):
-            for j in range(1, N+1):
-                for k in range(len(roads[j])):
-                    next, dist = roads[j][k]
-                    # print(f'next: {next}, dist: {dist}, distance: {distance}')
-                    if distance[next] > distance[j] + dist:
-                        distance[next] = distance[j] + dist
-                        
-                        if i == N-1:
-                            return True
-                for k in range(len(wormholes[j])):
-                    next, dist = wormholes[j][k]
-                    # print(f'next: {next}, dist: {dist}, distance: {distance}')
-                    if distance[next] > distance[j] + dist:
-                        distance[next] = distance[j] + dist
-                        
-                        if i == N-1:
-                            return True
-        return False
-    N, M, W = map(int, input().split()) # 지점, 도로, 웜홀
+def bellman_ford(start):
     INF = int(1e9)
-    roads = [[] * (N+1) for _ in range(N+1)]
-    wormholes = [[] * (N+1) for _ in range(N+1)]
+    dist = [INF] * (n + 1)
+    dist[start] = 0
     
-    
-    
-    for _ in range(M): # 도로는 양방향
-        s, e, t = map(int, input().split()) # 시작, 도착, 시간
+    for i in range(n):
+        for j in range(len(edges)):
+            cur, next_node, cost = edges[j]
+            
+            if dist[next_node] > dist[cur] + cost:
+                dist[next_node] = dist[cur] + cost
+                
+                if i == n-1:
+                    return True
+    return False
 
-        roads[s].append((e, t))
-        roads[e].append((s, t))
-        
-    for _ in range(W): # 웜홀은 단방향
-        s, e, t = map(int, input().split()) # 시작, 도착, 시간
-        
-        wormholes[s].append((e, -t))
-    if bf():
-        print('YES')
+for _ in range(int(input())):
+    n, m, w = map(int, input().split())
+    edges = []
+    
+    for _ in range(m):
+        s, e, t = map(int, input().split())
+        edges.append((s, e, t))
+        edges.append((e, s, t))
+    
+    for _ in range(w):
+        s, e, t = map(int, input().split())
+        edges.append((s, e, -t))
+    
+    if bellman_ford(1):
+        print("YES")
     else:
-        print('NO')
+        print("NO")
