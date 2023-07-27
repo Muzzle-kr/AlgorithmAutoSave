d55f9d50f1a985410af0b10d78be36f87adaa303
한주형 <91880235+Jureamer@users.noreply.github.com>
2023-07-27 10:23:04 +0900

    [Gold II] Title: 소가 길을 건너간 이유 7, Time: 204 ms, Memory: 116588 KB -BaekjoonHub

--- 백준/Gold/14461. 소가 길을 건너간 이유 7/소가 길을 건너간 이유 7.py
+++ 백준/Gold/14461. 소가 길을 건너간 이유 7/소가 길을 건너간 이유 7.py
@@ -1,4 +1,3 @@
-
 import heapq as hq
 import sys
 
@@ -6,7 +5,7 @@ def dijkstra():
     q = []
     hq.heappush(q, (0, 0, 0, 0))
     
-    INF = float('inf')
+    INF = sys.maxsize
     dist = [[INF] * N for _ in range(N)]
     dist[0][0] = 0
     ans = INF
@@ -16,31 +15,27 @@ def dijkstra():
         
         if dist[x][y] < d: continue
         
-        toN = (N - x - 1) + (N - y - 1)
+        remain = (N-1-x) + (N-1-y)
         
-        # 목적지까지 3칸 안에 갈 수 있다면
-        if toN <= 2:
-            ans = min(ans, d + toN * T)
+        if remain <= 2:
+            ans = min(ans, d + remain * T)
             
-        for i in range(16):
-            nx = x + dx[i]
-            ny = y + dy[i]
+        for k in range(16):
+            nx = x + dx[k]
+            ny = y + dy[k]
             
             if 0 <= nx < N and 0 <= ny < N:
-                cost = d + 3 * T + graph[nx][ny]
+                cost = graph[nx][ny] + d + 3 * T
                 
                 if dist[nx][ny] > cost:
                     dist[nx][ny] = cost
-                hq.heappush(q, (cost, c+1 , nx, ny))
-                
+                    hq.heappush(q, (cost, c + 1, nx, ny))
     return ans
-    
 input = sys.stdin.readline
 N, T = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(N)]
 
-dx = [0, 0, 1, -1, 0, 1, 2, 3, 2, 1, 0, -1, -2, -3, -2, -1]
-dy = [1, -1, 0, 0, 3, 2, 1, 0, -1, -2, -3, -2, -1, 0, 1, 2]
+dx = [1, -1,  0,  0,  0,  1,  2,  3,  2,  1,  0, -1, -2, -3, -2, -1]
+dy = [0,  0,  1, -1,  3,  2,  1,  0, -1, -2, -3, -2, -1,  0,  1,  2]
 
 print(dijkstra())
-
