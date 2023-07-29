2796eeaa777921a4a644b7214d2d282b45ff62af
한주형 <91880235+Jureamer@users.noreply.github.com>
2023-07-29 11:40:22 +0900

    [Platinum V] Title: 행성 터널, Time: 2316 ms, Memory: 191764 KB -BaekjoonHub

---
+++ 백준/Platinum/2887. 행성 터널/행성 터널.py
@@ -0,0 +1,57 @@
+def get_cost(a, b):
+    return min([abs(a[i]-b[i]) for i in range(3)])
+
+def get_parent(parents, x):
+    if parents[x] == x:
+        return x
+    return get_parent(parents, parents[x])
+
+def find_parent(parents, a, b):
+    a = get_parent(parents, a)
+    b = get_parent(parents, b)
+    if a == b:
+        return True
+    else:
+        return False
+
+def union_parent(parents, a, b):
+    a = get_parent(parents, a)
+    b = get_parent(parents, b)
+    if a < b:
+        parents[b] = a
+    else:
+        parents[a] = b
+        
+n = int(input())
+costs = [[] for _ in range(n)]
+x_list, y_list, z_list = [], [], []
+
+for i in range(n):
+    x, y, z = map(int, input().split())
+    x_list.append((x, i))
+    y_list.append((y, i))
+    z_list.append((z, i))
+
+x_list.sort(); y_list.sort(); z_list.sort();
+
+edges = []
+
+for cur_list in (x_list, y_list, z_list):
+    for i in range(1, n):
+        w1, a = cur_list[i]
+        w2, b = cur_list[i-1]
+        edges.append((abs(w1 - w2), a, b))
+
+edges.sort(reverse=True)
+
+parents = [i for i in range(n)]
+ans = 0
+cnt = n-1
+while cnt:
+    dist, a, b = edges.pop()
+    
+    if not find_parent(parents, a, b):
+        union_parent(parents, a, b)
+        ans += dist
+        cnt -= 1
+print(ans)
