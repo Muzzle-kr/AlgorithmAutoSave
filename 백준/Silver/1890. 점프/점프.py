1f943d543cdc2382e935c59e7539f887f00bf18d
한주형 <91880235+Jureamer@users.noreply.github.com>
2023-08-27 10:12:25 +0900

    [Silver I] Title: 점프, Time: 132 ms, Memory: 114488 KB -BaekjoonHub

---
+++ 백준/Silver/1890. 점프/점프.py
@@ -0,0 +1,20 @@
+N = int(input())
+graph = [list(map(int, input().split())) for _ in range(N)]
+
+dp = [[0] * N for _ in range(N)]
+dp[0][0] = 1
+
+for i in range(N):
+    for j in range(N):
+        if i == N-1 and j == N-1:
+            print(dp[-1][-1])
+            break
+        
+        cur_cnt = graph[i][j]
+        
+        if i + cur_cnt < N:
+            dp[i+cur_cnt][j] += dp[i][j]
+        
+        if j + cur_cnt < N:
+            dp[i][j+cur_cnt] += dp[i][j]
+            
