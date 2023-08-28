43b9f81eb969bee3a0dd1eca312eb756fae1c7a4
한주형 <91880235+Jureamer@users.noreply.github.com>
2023-08-28 10:56:53 +0900

    [Gold V] Title: 암호코드, Time: 132 ms, Memory: 114488 KB -BaekjoonHub

---
+++ 백준/Gold/2011. 암호코드/암호코드.py
@@ -0,0 +1,23 @@
+n = list(map(int, input()))
+l = len(n)
+dp = [0] * (l + 1)
+
+if n[0] == 0:
+    print(0)
+    exit()
+    
+n = [0] + n 
+dp[0] = dp[1] = 1
+
+
+for i in range(2, l+1):
+    if n[i] > 0:
+        dp[i] += dp[i-1]
+    
+    tmp = n[i-1] * 10 + n[i]
+    
+    if 10 <= tmp <= 26:
+        dp[i] += dp[i-2]
+
+print(dp[l] % 1000000)
+    
