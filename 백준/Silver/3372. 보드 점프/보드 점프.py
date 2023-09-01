0d9821b765a4b9e7ef3e8a9029c61a9acc97ef01
한주형 <91880235+Jureamer@users.noreply.github.com>
2023-09-01 09:23:20 +0900

    [Silver I] Title: 보드 점프, Time: 132 ms, Memory: 115408 KB -BaekjoonHub

--- 백준/Silver/3372. 보드 점프/보드 점프.py
+++ 백준/Silver/3372. 보드 점프/보드 점프.py
@@ -6,14 +6,15 @@ dp[0][0] = 1
 
 for i in range(N):
     for j in range(N):
+        
         if i == N-1 and j == N-1:
             print(dp[-1][-1])
             break
         
-        cur_cnt = graph[i][j]
+        cnt = graph[i][j]
         
-        if i + cur_cnt < N:
-            dp[i+cur_cnt][j] += dp[i][j]
+        if i + cnt < N:
+            dp[i+cnt][j] += dp[i][j]
         
-        if j + cur_cnt < N:
-            dp[i][j+cur_cnt] += dp[i][j]
+        if j + cnt < N:
+            dp[i][j+cnt] += dp[i][j]
