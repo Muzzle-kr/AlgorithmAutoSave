76609d9ebe4bd4cf45b0cf577973a8adcf01552d
한주형 <91880235+Jureamer@users.noreply.github.com>
2023-09-06 10:07:18 +0900

    [Silver II] Title: 창영이와 점프, Time: 148 ms, Memory: 125548 KB -BaekjoonHub

---
+++ 백준/Silver/22114. 창영이와 점프/창영이와 점프.py
@@ -0,0 +1,28 @@
+import sys
+input = sys.stdin.readline
+N, K = map(int,input().split())
+L = list(map(int,input().split()))
+i, j, cnt = 0, 0, 1
+check, answer, jump = 0, 0, 0
+
+while True:
+    if i == N - 1 or j == N - 1:
+        break
+    
+    if L[j] <= K:
+        cnt += 1
+        j += 1
+    else:
+        if jump == 0:
+            jump += 1
+            cnt += 1
+            check = j
+            j += 1
+        else:
+            i = check + 1
+            j = i
+            jump = 0
+            answer = max(answer, cnt)
+            cnt, check = 1, 0
+            
+print(max(cnt, answer))
