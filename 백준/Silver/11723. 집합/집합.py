2859c1d51c6920ff78f2926961bc52ab1c04a1f3
한주형 <91880235+Jureamer@users.noreply.github.com>
2023-09-10 10:09:50 +0900

    [Silver V] Title: 집합, Time: 5328 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.py
+++ 백준/Silver/11723. 집합/집합.py
@@ -1,34 +1,29 @@
 import sys
 input = sys.stdin.readline
+N = int(input())
+dict = {}
 
-arr = [0] * 21
-
-for _ in range(int(input())):
-    order = input().split()
+for _ in range(N):
+    operation = list(input().split())
+    order = operation[0]
     
-    if order[0] == "all" or order[0] == "empty":
-        for i in range(1,21):
-            arr[i] = 1 if order[0] == "all" else 0
+    if order == "add":
+        dict[int(operation[1])] = 1
+    elif order == "remove":
+        if int(operation[1]) in dict:
+            del dict[int(operation[1])]
+    elif order == "check":
+        if int(operation[1]) in dict:
+            print(1)
+        else:
+            print(0)
+    elif order == "toggle":
+        if int(operation[1]) in dict:
+            del dict[int(operation[1])]
+        else:
+            dict[int(operation[1])] = 1
+    elif order == "all":
+        dict = {i:1 for i in range(1,21)}
     else:
-        command = order[0]
-        num = int(order[1])
+        dict = {}
         
-        if command == "add":
-            if arr[num] == 0:
-                arr[num] = 1
-        
-        elif command == "remove":
-            if arr[num] == 1:
-                arr[num] = 0
-    
-        elif command == "check":
-            if arr[num] == 1:
-                print(1)
-            else:
-                print(0)
-    
-        elif command == "toggle":
-            if arr[num] == 1:
-                arr[num] = 0
-            else:
-                arr[num] = 1
