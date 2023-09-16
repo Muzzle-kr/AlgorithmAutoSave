3cbc41cbc3253b11e1998427556be053c2487cda
한주형 <91880235+Jureamer@users.noreply.github.com>
2023-09-16 21:46:26 +0900

    [Bronze II] Title: 벨트, Time: 116 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Bronze/10834. 벨트/벨트.py
+++ 백준/Bronze/10834. 벨트/벨트.py
@@ -7,7 +7,7 @@ for _ in range(M):
     # c == 1: 꼬인형태, c == 0: 안 꼬인 형태
     a, b, c = map(int, input().split()) 
     
-    speed_ratio *= b/a
+    speed_ratio *= (b/a)
     
     if c == 1:
         direction = 1 - direction
