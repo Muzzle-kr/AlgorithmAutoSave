9638a848f8659f69cef09828f625cc3ab4f1c900
한주형 <wngud4950@gmail.com>
2023-12-20 10:43:50 +0900

    [Gold V] Title: A와 B 2, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Gold/12919. A와 B 2/A와 B 2.py
@@ -0,0 +1,19 @@
+S = list(input())
+T = list(input())
+
+def dfs(t):
+    if t == S:
+        print(1)
+        exit()
+    
+    if len(t) == 0:
+        return
+    
+    if t[-1] == 'A':
+        dfs(t[:-1])
+    
+    if t[0] == 'B':
+        dfs(t[1:][::-1])
+    
+dfs(T)
+print(0)
