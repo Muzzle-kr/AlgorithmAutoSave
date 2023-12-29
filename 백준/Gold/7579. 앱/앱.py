980f54c0f459baea70e8e4cedeeb700b927ae91b
한주형 <wngud4950@gmail.com>
2023-12-29 10:41:47 +0900

    [Gold III] Title: 앱, Time: 144 ms, Memory: 114384 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.py
+++ 백준/Gold/7579. 앱/앱.py
@@ -1,19 +1,17 @@
-N, M = map(int, input().split())
-bytes = [0] + list(map(int, input().split())) #byte
-costs = [0] + list(map(int, input().split())) #cost
-dp = [[0 for _ in range(sum(costs)+1)] for _ in range(N+1)] # i번째 앱까지 비용 j로, 얻을 수 있는 최대 메모리를 저장
-result = sum(costs) #열의 최댓값
+N, M = map(int, input().split()) 
+memory = list(map(int, input().split()))
+c = list(map(int, input().split())) 
+dp = [[0 for _ in range(sum(c)+1)] for _ in range(N+1)] 
+# dp[i][j] => 1~i번째 앱까지, j의 cost로 확보 가능한 메모리의 최대
 
-for i in range(1, N+1):
-    byte = bytes[i]
-    cost = costs[i] 
+for i in range(1, N+1):    
+    for j in range(0, sum(c)+1):        
+        if j >= c[i-1]:            
+            dp[i][j] = max(dp[i-1][j], dp[i-1][j-c[i-1]]+memory[i-1])        
+        else:            
+            dp[i][j] = dp[i-1][j] 
     
-    for j in range(1, sum(costs)+1):
-        if j < cost:
-            dp[i][j] = dp[i-1][j]
-        else:
-            dp[i][j] = max(dp[i-1][j], dp[i-1][j-cost] + byte)
-        
-        if dp[i][j] >= M:
-            result = min(result, j)
-print(result)
+    for i, mem in enumerate(dp[N]):    
+        if mem >= M:        
+            print(i)        
+            break
