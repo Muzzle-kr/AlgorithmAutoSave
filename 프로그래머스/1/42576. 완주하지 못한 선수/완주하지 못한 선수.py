f05c6729150843211f3d6338e3f86c3a8e1f382c
한주형 <wngud4950@gmail.com>
2023-12-31 13:24:59 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 78.07 ms, Memory: 26.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -0,0 +1,11 @@
+def solution(participant, completion):
+    participant.sort()
+    completion.sort()
+    
+    for i in range(len(participant)):
+        if i == len(participant) - 1:
+            return participant[i]
+        if participant[i] != completion[i]:
+            return participant[i]
+
+    return 0
