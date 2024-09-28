da9f4363c1c9e9473746a680d66e65011fd9c2d9
한주형 <wngud4950@gmail.com>
2024-09-28 11:34:02 +0900

    [Silver II] Title: 창고 다각형, Time: 100 ms, Memory: 110272 KB -BaekjoonHub

--- 백준/Silver/2304. 창고 다각형/창고 다각형.py
+++ 백준/Silver/2304. 창고 다각형/창고 다각형.py
@@ -1,34 +1,38 @@
-n = int(input())
+N = int(input())
+arr = sorted([list(map(int, input().split())) for _ in range(N)], key=lambda x: x[0])
 
-pillars = sorted([list(map(int, input().split())) for _ in range(n)], key=lambda x: x[0])
+max_y = 0
+max_idx = 0
 
-max_height = 0
-max_height_idx = 0
-
-for i in range(n):
-    if pillars[i][1] > max_height:
-        max_height = pillars[i][1]
-        max_height_idx = i
-        
-ans = 0
-prev_w, prev_h = pillars[0][0], pillars[0][1]
-
-for i in range(1, max_height_idx + 1):
-    w, h = pillars[i][0], pillars[i][1]
+for i in range(N):
+    x, y = arr[i][0], arr[i][1]
     
-    if h >= prev_h:
-        ans += (w - prev_w) * prev_h
-        prev_w, prev_h = w, h
-
-prev_w, prev_h = pillars[-1][0], pillars[-1][1]
-
-for i in range(n-2, max_height_idx-1, -1):
-    w, h = pillars[i][0], pillars[i][1]
+    if y > max_y:
+        max_y = y
+        max_idx = i
     
-    if h >= prev_h:
-        ans += (prev_w - w) * prev_h
-        prev_w, prev_h = w, h
+answer = 0
 
-print(ans + max_height)
+# 왼쪽에서 시작
+prev_x, prev_y = arr[0][0], arr[0][1]
+
+for i in range(1, max_idx + 1):
+    x, y = arr[i]
+
+    # 높은 기둥을 찾으면 계산하고 이전 좌표를 업데이트한다.
+    if y >= prev_y:
+        answer += prev_y * abs(x - prev_x)
+        prev_x, prev_y = x, y
+
+# 오른쪽에서 시작
+prev_x, prev_y = arr[-1][0], arr[-1][1]
+
+for i in range(N-2, max_idx-1, -1):
+    x, y = arr[i]
+
+    # 높은 기둥을 찾으면 계산하고 이전 좌표를 업데이트한다.
+    if y >= prev_y:
+        answer += prev_y * abs(x - prev_x)
+        prev_x, prev_y = x, y
     
-    
+print(answer + max_y)
