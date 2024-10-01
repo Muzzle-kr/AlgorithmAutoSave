55059a3f04e81286a6656bc2a29fcff613c6634b
한주형 <wngud4950@gmail.com>
2024-10-01 18:33:48 +0900

    [Gold III] Title: 아기 상어, Time: 132 ms, Memory: 115632 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.py
+++ 백준/Gold/16236. 아기 상어/아기 상어.py
@@ -1,10 +1,23 @@
 from collections import deque
 
-def bfs(sx, sy, initial_level, initial_feed, initial_time):       
+N = int(input())
+aquarium = [list(map(int, input().split())) for _ in range(N)]
+
+def find_shark():
+    for i in range(N):
+        for j in range(N):
+            if aquarium[i][j] == 9:
+                return i, j
+
+sx, sy = find_shark()
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+
+def bfs(sx, sy, init_level, init_feed, init_time):
     q = deque()
-    q.append((sx, sy, initial_level, initial_feed, initial_time))
-    visit = [[0] * N for _ in range(N)]
-    visit[sx][sy] = 1
+    q.append((sx, sy, init_level, init_feed, init_time))
+    visited = [[False] * N for _ in range(N)]
+    visited[sx][sy] = True
     
     feeds = []
     min_distance = int(1e9)
@@ -16,55 +29,39 @@ def bfs(sx, sy, initial_level, initial_feed, initial_time):
             break
         
         for i in range(4):
-            nx, ny = x + dx[i], y + dy[i]
-            if 0 <= nx < N and 0 <= ny < N and not visit[nx][ny] and graph[nx][ny] <= level:
-                visit[nx][ny] = 1
+            nx = dx[i] + x
+            ny = dy[i] + y
+            
+            if 0 <= nx < N and 0 <= ny < N and not visited[nx][ny] and aquarium[nx][ny] <= level:
+                visited[nx][ny] = True
                 
                 # 먹이를 찾은 경우
-                if graph[nx][ny] < level and graph[nx][ny] != 0:
+                if aquarium[nx][ny] < level and aquarium[nx][ny] != 0:
+                    
                     if min_distance > time + 1:
                         feeds = [(nx, ny)]
                         min_distance = time + 1
                     elif min_distance == time + 1:
                         feeds.append((nx, ny))
-                        
-                elif graph[nx][ny] == level or graph[nx][ny] == 0:
+                
+                elif aquarium[nx][ny] == level or aquarium[nx][ny] == 0:
                     q.append((nx, ny, level, feed, time + 1))
-    
+                
     if feeds:
-        # 먹이가 여러개인 경우 가장 위쪽, 가장 왼쪽에 있는 먹이를 먹는다.
+        
+        # 먹이가 여러개라면 정렬해서 먹음
         if len(feeds) > 1:
-            feeds.sort(key=lambda x: (x[0], x[1]))
+            feeds.sort(key=lambda x : (x[0], x[1]))
         
         fx, fy = feeds[0]
-        graph[fx][fy] = 9
-        graph[sx][sy] = 0
+        aquarium[fx][fy] = 9
+        aquarium[sx][sy] = 0
+        
         if feed + 1 == level:
-            return bfs(fx, fy, level+1, 0, min_distance)
+            return bfs(fx, fy, level + 1, 0, min_distance)
         else:
             return bfs(fx, fy, level, feed + 1, min_distance)
     else:
-        if level == 2 and feed == 0:
-            return 0
-        else:
-            return initial_time
+        return init_time
             
-        
-
-N = int(input())
-graph = [list(map(int, input().split())) for _ in range(N)]
-sx, sy = 0, 0
-
-dx = [0, 0, -1, 1]
-dy = [-1, 1, 0, 0]
-    
-for i in range(N):
-    try:
-        if graph[i].index(9) != -1:
-            sx, sy = i, graph[i].index(9)
-            break
-    except:
-        continue
-    
-
 print(bfs(sx, sy, 2, 0, 0))
