c29f63fa5ffaf1ec5c734a5e0a728f843712bba7
한주형 <wngud4950@gmail.com>
2024-10-03 21:23:33 +0900

    [Silver III] Title: 퇴사, Time: 88 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.py
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -1,31 +1,14 @@
-def consultation(days, earn, current_day):
-    if current_day == n:
-        earns.append(earn)
-        return 
-    
-    
-    consultation_days, consultation_earn = consultations[current_day]
-    
-    # 이미 상담을 진행 중이라면 다음 날로 넘기기
-    if days > 0:    
-        consultation(days-1, earn, current_day+1)
-        return
-    
-    # 남은 날짜보다 상담시간이 길면 상담을 할 수 없다.
-    if current_day + consultation_days > n:
-        consultation(days-1, earn, current_day+1)
-        return
-    
-    # 상담하기
-    consultation(consultation_days-1, earn+consultation_earn, current_day+1)
-    # 상담하지 않기
-    consultation(days-1, earn, current_day+1)
-    return
-    
-n = int(input())
-consultations = [list(map(int, input().split())) for _ in range(n)]
-earns = []
+N = int(input())
 
-consultation(0, 0, 0)
+items = [list(map(int, input().split())) for _ in range(N)]
+dp = [0] * (N+1)
 
-print(max(earns))
+for i in range(N-1, -1, -1):
+    time, earn = items[i]
+    
+    if i + items[i][0] > N:
+        dp[i] = dp[i+1]
+    else:
+        dp[i] = max(dp[i+1], dp[i+time] + earn)
+
+print(dp[0])
