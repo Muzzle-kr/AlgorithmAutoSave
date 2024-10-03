e3196b598031b36ea58495eb0f23d18fa61e918e
한주형 <wngud4950@gmail.com>
2024-10-03 15:33:37 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 92 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.py
+++ 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.py
@@ -1,24 +1,19 @@
-def dfs(depth, cnt, lemon, bad):
-    global result
-
-    # 가지 치기
-    if cnt != 0:
-        if abs(lemon - bad) < result:
-            result = abs(lemon - bad)
-
-    # 기저
-    if depth == N:
-        return
-
-    # 재귀
-    dfs(depth + 1, cnt + 1, lemon * cuisine[depth][0], bad + cuisine[depth][1])  # 깊이와 개수 모두 증가시켜준다.
-    dfs(depth + 1, cnt, lemon, bad)  # 깊이만 증가시키고 개수는 증가시키지 않는다.
-
-
 N = int(input())
-cuisine = [list(map(int, input().split())) for i in range(N)]
-arr = []
-result = 1 << 100  # 결과값을 처음에 아주 큰 값으로 초기화
+ingres = [list(map(int, input().split())) for _ in range(N)]
+answer = float('inf')
 
-dfs(0, 0, 1, 0)
-print(result)
+def recur(idx, sour, bitter, use):
+    global answer
+    if idx == N:
+        if use > 0:
+            answer = min(answer, abs(sour - bitter))
+        return
+    
+    # 사용
+    recur(idx+1, sour * ingres[idx][0], bitter + ingres[idx][1], use + 1)
+    
+    # 미사용
+    recur(idx+1, sour, bitter, use)
+
+recur(0, 1, 0, 0)
+print(answer)
