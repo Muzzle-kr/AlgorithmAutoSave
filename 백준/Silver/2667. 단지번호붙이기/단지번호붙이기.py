32340a7502ec17f9f0565726de6c27c4fd73059a
한주형 <wngud4950@gmail.com>
2024-10-05 08:58:13 +0900

    [Silver I] Title: 단지번호붙이기, Time: 108 ms, Memory: 113164 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,50 +1,42 @@
-import sys
-input = sys.stdin.readline
-sys.setrecursionlimit(10**5) 
+from collections import deque
 
+N = int(input())
+estate = [list(input()) for _ in range(N)]
+visited = [[False] * N for _ in range(N)]
 
-def dfs(x, y):
-    
-    dx = [0, 0, 1, -1]
-    dy = [1, -1, 0, 0]
-    cnt = 1
-    
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        
-        if 0 <= nx < size and 0 <= ny < size:
-            if check[nx][ny] == 0 and apartments[nx][ny] == 1:
-                check[nx][ny] = numberOfComplex
-                cnt += dfs(nx, ny)
-    return cnt
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
 
-
-apartments = []
-size = int(input())
-check = [[0]* size for _ in range(size)]
-numberOfComplex = 0
 answer = []
-for i in range(size):
-    apartments.append(list(map(int, list(input()[:-1]))))
 
-
-for i in range(size):
-    for j in range(size):
-        if check[i][j] == 0 and apartments[i][j] == 1:
-            # 단지 수 증가
-            numberOfComplex += 1
+def bfs(i, j):
+    q = deque()
+    q.append((i, j))
+    visited[i][j] = True
+    count = 0
+    
+    while q:
+        x, y = q.popleft()
+        count += 1
+        
+        for k in range(4):
+            nx = dx[k] + x
+            ny = dy[k] + y
             
-            count = dfs(i, j)
-            if count == 1:
-                answer.append(count)    
-            else:
-                answer.append(count-1)
-            
-          
-answer.sort()  
+            if 0 <= nx < N and 0 <= ny < N and estate[nx][ny] == "1" and not visited[nx][ny]:
+                visited[nx][ny] = True
+                q.append((nx, ny))
+    return count
+        
+        
+for i in range(N):
+    for j in range(N):
+        if estate[i][j] == "1" and not visited[i][j]:
+            answer.append(bfs(i, j))
 
-print(numberOfComplex)
-# print(answer)
-for i in answer:
-    print(i)
+answer.sort()
+
+print(len(answer))
+
+for a in answer:
+    print(a)
