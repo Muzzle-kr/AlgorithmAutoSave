3b5f3f33b8ead647a77b40bb62573d7856642f6a
한주형 <wngud4950@gmail.com>
2024-10-05 08:10:13 +0900

    [Silver I] Title: 미로 탐색, Time: 128 ms, Memory: 114460 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,30 +1,29 @@
 from collections import deque
 
-dx = (1, 0, -1, 0)
-dy = (0, 1, 0, -1)
 N, M = map(int, input().split())
-board = [input() for _ in range(N)]
+maze = [list(input()) for _ in range(N)]
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
 
+def bfs(i, j):
 
-def chk_valid_coordinate(y, x):
-  return 0 <= x < M and 0 <= y < N
-
-def bfs(y, x, d):
-  chkBoard = [[False] * M for _ in range(N)]
-  chkBoard[0][0] = True
-  dq = deque()
-  dq.append((0, 0, 1))
-  while dq:
-    y, x, d = dq.popleft()
-    if y == N - 1 and x == M - 1:
-      print(d)
-      
-    nd = d + 1
-    for i in range(4):
-      nx = x + dx[i]
-      ny = y + dy[i]
-      if chk_valid_coordinate(ny, nx) and board[ny][nx] == '1' and not chkBoard[ny][nx]:
-        chkBoard[ny][nx] = True
-        dq.append((ny, nx, nd))
-
-bfs(0, 0, 0)
+    q = deque()
+    q.append((i, j, 1))
+    visited = [[False] * M for _ in range(N)]
+    visited[i][j] = True
+    while q:
+        x, y, count = q.popleft()
+        
+        if x == N-1 and y == M-1:
+            print(count)
+            break
+        
+        for i in range(4):
+            nx = dx[i] + x
+            ny = dy[i] + y
+        
+            if 0 <= nx < N and 0 <= ny < M and not visited[nx][ny] and maze[nx][ny] == "1":
+                visited[nx][ny] = True
+                q.append((nx, ny, count+1))
+                
+bfs(0, 0)
