72839526096486e47b92681b34298247b767f4e6
한주형 <wngud4950@gmail.com>
2024-10-05 08:16:39 +0900

    [Silver I] Title: 음식물 피하기, Time: 156 ms, Memory: 115052 KB -BaekjoonHub

--- 백준/Silver/1743. 음식물 피하기/음식물 피하기.py
+++ 백준/Silver/1743. 음식물 피하기/음식물 피하기.py
@@ -1,41 +1,41 @@
-import sys
-input = sys.stdin.readline
-sys.setrecursionlimit(10**5) 
+from collections import deque
 
+N, M, K = map(int, input().split())
+condo = [[0] * M for _ in range(N)]
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
 
-def dfs(x, y):
-
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, -1, 1]
-    cnt = 1
+# 콘도에 음식물 쓰레기 투척
+for _ in range(K):
+    x, y = map(int, input().split())
+    condo[x-1][y-1] = 1
     
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
+visited = [[False] * M for _ in range(N)] 
+
+def bfs(i, j):
+    visited[i][j] = True
+    q = deque()
+    q.append((i, j))
+    count = 0
+    
+    while q:
+        x, y = q.popleft()
+        count += 1
         
-        if 0 <= nx < n and 0 <= ny < m:    
-            if corridor[nx][ny] == 1:
-                corridor[nx][ny] = 0
-                
-                cnt += dfs(nx, ny)
-    return cnt
-                
-n, m, numberOfTrash = map(int, input().split())
-corridor = [[0 for _ in range(m)] for _ in range(n)]
+        for k in range(4):
+            nx = dx[k] + x
+            ny = dy[k] + y
+            
+            if 0 <= nx < N and 0 <= ny < M and condo[nx][ny] == 1 and not visited[nx][ny]:
+                visited[nx][ny] = True
+                q.append((nx, ny))
+    return count    
+
 answer = 0
 
-for j in range(numberOfTrash):
-    x, y = map(int, input().split())
-    corridor[x-1][y-1] = 1
-
-# for i in corridor:
-#     print(i)
-    
-for x in range(n):
-    count = 0
-    for y in range(m):
-        if corridor[x][y] == 1:
-            corridor[x][y] = 0
-            answer = max(answer, dfs(x, y))
+for i in range(N):
+    for j in range(M):
+        if condo[i][j] == 1 and not visited[i][j]:
+            answer = max(answer, bfs(i, j))
 
 print(answer)
