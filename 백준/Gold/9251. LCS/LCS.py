747d16338d05a70f6548b0f673959d81e7fdf8c4
한주형 <wngud4950@gmail.com>
2024-10-06 13:00:44 +0900

    [Gold V] Title: LCS, Time: 116 ms, Memory: 118204 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -1,17 +1,13 @@
-import sys
-input = sys.stdin.readline
+str1 = input()
+str2 = input()
 
-word1 = input().strip()
-word2 = input().strip()
-word_length1 = len(word1)
-word_length2 = len(word2)
+dp = [[0] * (len(str2)+1) for _ in range(len(str1)+1)]
 
-dp = [[0] * (word_length2+1) for _ in range(word_length1+1)]
-
-for i in range(1, word_length1+1):
-    for j in range(1, word_length2+1):
-        if word1[i-1] == word2[j-1]:
+for i in range(1, len(str1)+1):
+    for j in range(1, len(str2)+1):
+        if str1[i-1] == str2[j-1]:
             dp[i][j] = dp[i-1][j-1] + 1
         else:
             dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+            
 print(dp[-1][-1])
