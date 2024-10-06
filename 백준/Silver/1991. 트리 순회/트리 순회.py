9f5714dcf51c1c124b2f672b0ff96ff616618bcd
한주형 <wngud4950@gmail.com>
2024-10-06 13:23:58 +0900

    [Silver I] Title: 트리 순회, Time: 88 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.py
+++ 백준/Silver/1991. 트리 순회/트리 순회.py
@@ -1,50 +1,56 @@
-n = int(input())
-dict = {}
-forward = ""
-middle = ""
-backward = ""
+N = int(input())
+tree = [[] for _ in range(27)]
 
-def 전위순회(node):
-    global forward
-    forward += node
+for _ in range(N):
+    a, s1, s2 = input().split()
     
-    if dict[node][0] != '.':
-        전위순회(dict[node][0])
-    
-    if dict[node][1] != '.':
-        전위순회(dict[node][1])
-    
-def 중위순회(node):
-    global middle
-    
-    if dict[node][0] != '.':
-        중위순회(dict[node][0])
-        
-    middle += node
-    
-    if dict[node][1] != '.':
-        중위순회(dict[node][1])
-    
+    a = ord(a) - 65
+    s1 = ord(s1) - 65 if s1 != '.' else None
+    s2 = ord(s2) - 65 if s2 != '.' else None
 
-def 후위순회(node):
-    global backward
+    tree[a] = [s1, s2]
     
-    if dict[node][0] != '.':
-        후위순회(dict[node][0])
-    
-    if dict[node][1] != '.':
-        후위순회(dict[node][1])
-    
-    backward += node
-    
-for _ in range(n):
-    node, left, right = input().split()
-    dict[node] = [left, right]
+pre_order_answer = ""
+in_order_answer = ""
+post_order_answer = ""
 
-전위순회('A')
-중위순회('A')
-후위순회('A')
+def pre_order(idx):
+    global pre_order_answer
+    
+    if idx is None:
+        return
 
-print(forward)
-print(middle)
-print(backward)
+    s1, s2 = tree[idx]
+    pre_order_answer += chr(idx + 65)
+    pre_order(s1)
+    pre_order(s2)
+
+def in_order(idx):
+    global in_order_answer
+    
+    if idx is None:
+        return
+    
+    s1, s2 = tree[idx]
+    in_order(s1)
+    in_order_answer += chr(idx + 65)
+    in_order(s2)
+
+def post_order(idx):
+    global post_order_answer
+    
+    if idx is None:
+        return
+    
+    s1, s2 = tree[idx]
+    post_order(s1)
+    post_order(s2)
+    post_order_answer += chr(idx + 65)
+
+pre_order(0)
+in_order(0)
+post_order(0)
+
+print(pre_order_answer)
+print(in_order_answer)
+print(post_order_answer)
