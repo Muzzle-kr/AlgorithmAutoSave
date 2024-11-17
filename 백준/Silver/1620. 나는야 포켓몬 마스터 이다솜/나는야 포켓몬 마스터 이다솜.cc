aa3979bd873d77ef32419c9eee20c0944be81528
한주형 <wngud4950@gmail.com>
2024-11-17 14:32:58 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 108 ms, Memory: 25140 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
@@ -10,16 +10,15 @@ bool isDigit(const string& str) {
 }
 
 int main() {
-    ios::sync_with_stdio(false); // 입출력 비동기화 비활성화
-    cin.tie(nullptr);            // cout과 cin의 묶음 해제
+    ios::sync_with_stdio(false);
+    cin.tie(0);
     
     unordered_map<string, int> poketmonMapNameIndex;
     unordered_map<int, string> poketmonMapIndexName;
     int n, m;
     
     cin >> n >> m;
-
-    // 포켓몬 이름과 번호를 맵에 저장
+    
     for (int i = 1; i <= n; i++) {
         string name;
         cin >> name;
@@ -27,16 +26,13 @@ int main() {
         poketmonMapIndexName[i] = name;
     }
     
-    // 질의 처리
     for (int i = 0; i < m; i++) {
         string question;
         cin >> question;
         
         if (isDigit(question)) {
-            // 숫자일 경우 번호를 사용해 이름 출력
             cout << poketmonMapIndexName[stoi(question)] << "\n";
         } else {
-            // 문자열일 경우 이름을 사용해 번호 출력
             auto it = poketmonMapNameIndex.find(question);
             if (it != poketmonMapNameIndex.end()) {
                 cout << it->second << "\n";
