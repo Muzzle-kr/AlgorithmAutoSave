2b68d9a008e44ca28a81fa9d48e06bae44fa41d6
한주형 <wngud4950@gmail.com>
2024-11-18 19:22:47 +0900

    [Silver I] Title: 안전 영역, Time: 24 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.cc
+++ 백준/Silver/2468. 안전 영역/안전 영역.cc
@@ -8,8 +8,9 @@ typedef vector<vector<int>> vvi;
 
 int dx[] = {0, 0, -1, 1};
 int dy[] = {-1, 1, 0, 0};
+int n;
 
-void bfs(const vvi& v, vvi& visited, int i, int j, int height, int n) {
+void bfs(const vvi & v, vvi & visited, int i, int j, int height) {
     queue<tuple<int, int>> q;
     visited[i][j] = 1;
 
@@ -20,10 +21,9 @@ void bfs(const vvi& v, vvi& visited, int i, int j, int height, int n) {
         tie(x, y) = q.front();
         q.pop();
 
-        for (int k = 0; k < 4; k++) {
-            int tx = x + dx[k];
-            int ty = y + dy[k];
-
+        for (int i = 0; i < 4; i++) {
+            int tx = x + dx[i];
+            int ty = y + dy[i];
 
             if (tx >= 0 && tx < n && ty >= 0 && ty < n && v[tx][ty] > height && visited[tx][ty] == 0) {
                 visited[tx][ty] = 1;
@@ -32,29 +32,25 @@ void bfs(const vvi& v, vvi& visited, int i, int j, int height, int n) {
         }
     }
 }
-
-
-int findSafeZone(int height, const vvi& v, int n) {
-    vvi visited(n, vector<int>(n, 0));
+int findSafeZone(int height, const vvi & v) {
+    vvi visited(n, vector<int>(n));
     int count = 0;
 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             if (visited[i][j] == 0 && v[i][j] > height) {
-                bfs(v, visited, i, j, height, n);
+                bfs(v, visited, i, j, height);
                 count++;
             }
         }
     }
     return count;
-}
+};
 
 int main() {
-    int n;
     cin >> n;
 
     vvi v(n, vector<int>(n));
-
 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
@@ -64,13 +60,9 @@ int main() {
 
     int result = 0;
 
-
     for (int height = 0; height < 101; height++) {
-        result = max(result, findSafeZone(height, v, n));
+        result = max(result, findSafeZone(height, v));
     }
 
-
     cout << result << endl;
-
-    return 0;
 }
