28bfaa441a183da77259950d544d747a564389cb
한주형 <wngud4950@gmail.com>
2024-11-23 08:36:39 +0900

    [Silver V] Title: 영화감독 숌, Time: 56 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.cc
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.cc
@@ -1,36 +1,23 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
-
-vector<int> result = {666};
-
-bool includeDemonNumber(string s) {
-    return s.find("666") != string::npos;
-}
 
 int main() {
     int n;
     cin >> n;
 
-    if (n == 0) {
-        cout << result[0] << endl;
-        return 0;
-    }
+    int count = 0;
 
-    for (int i = 1; i < n; i++) {
-        int start = result[i-1] + 1;
+    for (int i = 665; i < 1000000000; i++) {
+        if (to_string(i).find("666") != string::npos) {
+            count++;
+        }
 
-        while (true) {
-            if (includeDemonNumber(to_string(start))) {
-                result.push_back(start);
-                break;
-            }
-            start++;
+        if (count == n) {
+            cout << i << endl;
+            return 0;
         }
     }
-
-    cout << result[n-1] << endl;
     return 0;
 }
