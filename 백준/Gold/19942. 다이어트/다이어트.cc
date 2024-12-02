c8f642fc59d48ed90a8a621caa1e6c83d6776d6a
한주형 <wngud4950@gmail.com>
2024-12-02 22:25:09 +0900

    [Gold IV] Title: 다이어트, Time: 12 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/19942. 다이어트/다이어트.cc
+++ 백준/Gold/19942. 다이어트/다이어트.cc
@@ -1,36 +1,30 @@
 #include <iostream>
 #include <vector>
-#include <limits>
-#include <bitset>
+
 using namespace std;
 
 int n;
 int a, b, c, d;
-int answer = numeric_limits<int>::max(); // 초기값 설정
-vector<int> answer_combination; // 최적의 조합을 저장
+int answer = 1e9;
+vector<int> answer_combination;
 
-void distinguish(int combi, const vector<vector<int>> &matrix) {
+void distinguish(int combi, const vector<vector<int>> & matrix) {
     vector<int> total_arr(5, 0);
     vector<int> current_combination;
-
-    // 현재 조합에서 선택된 식재료를 확인
     for (int i = 0; i < n; i++) {
         if (combi & (1 << i)) {
-            current_combination.push_back(i + 1); // 식재료 번호 저장
+            current_combination.push_back(i + 1);
             for (int j = 0; j < 5; j++) {
                 total_arr[j] += matrix[i][j];
             }
         }
     }
 
-    // 최소 영양소 조건을 만족하는지 확인
     if (total_arr[0] >= a && total_arr[1] >= b && total_arr[2] >= c && total_arr[3] >= d) {
         if (answer > total_arr[4]) {
-            // 더 낮은 비용 발견
             answer = total_arr[4];
             answer_combination = current_combination;
         } else if (answer == total_arr[4] && current_combination < answer_combination) {
-            // 비용이 동일한 경우 사전순으로 더 빠른 조합 선택
             answer_combination = current_combination;
         }
     }
@@ -48,21 +42,17 @@ int main() {
         }
     }
 
-    // 모든 조합 탐색
-    for (int combi = 1; combi < (1 << n); combi++) {
-        distinguish(combi, matrix);
+    for (int i = 1; i < (1 << n); i++) {
+        distinguish(i, matrix);
     }
 
-    // 결과 출력
-    if (answer == numeric_limits<int>::max()) {
+    if (answer == 1e9) {
         cout << "-1" << endl;
     } else {
         cout << answer << endl;
         for (int i : answer_combination) {
-            cout << i << " ";
+                cout << i << " ";
         }
-        cout << endl; // 출력 형식에 줄바꿈 추가
     }
-
     return 0;
 }
