85b8e47ba9aee45ece374e6f9d280e0c8cf8c6be
한주형 <wngud4950@gmail.com>
2024-12-19 13:21:34 +0900

    [Silver IV] Title: 암기왕, Time: 1820 ms, Memory: 72380 KB -BaekjoonHub

---
+++ 백준/Silver/2776. 암기왕/암기왕.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+int t;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> t;
+
+    for (int i = 0; i < t; i++) {
+        int n, m;
+        vector<int> arr1;
+        vector<int> arr2;
+
+        cin >> n;
+        map<int, int> check_hash;
+
+        for (int j = 0; j < n; j++) {
+            int temp;
+            cin >> temp;
+            if (check_hash[temp] == 0) {
+                check_hash[temp] = 1;
+            }
+        }
+
+        cin >> m;
+        for (int j = 0; j < m; j++) {
+            int temp;
+            cin >> temp;
+
+            if (check_hash[temp] == 1) {
+                cout << 1 << "\n";
+            } else {
+                cout << 0 << "\n";
+            }
+        }
+    }
+}
