7133e2028bc9ef46b019a4e9cbdf15e611a85bcd
한주형 <wngud4950@gmail.com>
2025-01-13 21:32:16 +0900

    [Bronze I] Title: 색종이, Time: 52 ms, Memory: 5980 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/10163. 색종이/색종이.cc
+++ 백준/Bronze/10163. 색종이/색종이.cc
@@ -5,7 +5,7 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-    vector<vector<int>> v(104, vector<int>(104, -1));
+    vector<vector<int>> v(1004, vector<int>(1004, -1));
 
     for (int i = 0; i < n; i++) {
         int a, b, c, d;
@@ -19,17 +19,18 @@ int main() {
     }
 
     vector<int> area(n, 0);
-
-    for (int j = 0; j < 104; j++) {
-        for (int k = 0; k < 104; k++) {
+    for (int j = 0; j < 1004; j++) {
+        for (int k = 0; k < 1004; k++) {
             if (v[j][k] != -1) {
                 area[v[j][k]]++;
             }
         }
     }
 
+
     for (int i = 0; i < n; i++) {
         cout << area[i] << endl;
     }
+
     return 0;
 }
