2455af1e90592d7449b728022d32adef0825b9fe
한주형 <wngud4950@gmail.com>
2025-01-15 05:51:58 +0900

    [Bronze IV] Title: 줄번호, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/4470. 줄번호/줄번호.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    cin.ignore();
+
+    for (int i = 0; i < n; i++) {
+        string s;
+        getline(cin, s);
+
+        cout << i + 1 << ". " + s << endl;
+    }
+    return 0;
+}
