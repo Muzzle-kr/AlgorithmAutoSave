bfd94c70ea2235bdc165de3814765978ab0a0c9a
한주형 <wngud4950@gmail.com>
2025-01-23 08:38:11 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int N, K;
+    cin >> N >> K;
+
+    for (int i = 0; i < N; i++) {
+        int A, B;
+        cin >> A >> B;
+    }
+
+    cout << "비와이" << endl;
+
+    return 0;
+}
