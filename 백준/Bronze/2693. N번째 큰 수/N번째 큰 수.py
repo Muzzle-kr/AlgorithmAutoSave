43a7493e4b10fc60e2eec20372a9b7e0c15ffb5a
한주형 <wngud4950@gmail.com>
2025-02-11 05:37:18 +0900

    [Bronze I] Title: N번째 큰 수, Time: 148 ms, Memory: 110708 KB -BaekjoonHub

--- 백준/Bronze/2693. N번째 큰 수/N번째 큰 수.py
+++ 백준/Bronze/2693. N번째 큰 수/N번째 큰 수.py
@@ -1,5 +1,4 @@
-for _ in range(int(input())):
+for i in range(int(input())):
     arr = list(map(int, input().split()))
-    arr.sort(reverse = True)
-    
+    arr.sort(reverse=True)
     print(arr[2])
