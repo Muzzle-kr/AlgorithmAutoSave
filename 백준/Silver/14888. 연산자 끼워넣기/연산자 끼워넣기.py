65c42f9b5ffe274a5c95f99be3224a75f2841919
한주형 <wngud4950@gmail.com>
2025-02-12 21:22:21 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 2508 ms, Memory: 623132 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,35 +1,39 @@
-import sys
 from itertools import permutations
+n = int(input())
 
-input = sys.stdin.readline
-N = int(input())
-arr = list(map(int, input().split()))
-plus, minus, mul, div = map(int, input().split())
-total = plus + minus + mul + div
-signs = (['+']*plus) + (['-']*minus) + (['*']*mul) + (['//']*div)
-permutation = set(permutations(signs, total))
+numbers = list(map(int, input().split()))
+operators = list(map(int, input().split()))
+operators_list = []
+for op in range(4):
+    for _ in range(operators[op]):
+        operators_list.append(op)
 
-min = sys.maxsize
-max = -sys.maxsize
+permutations_list = list(permutations(operators_list, len(operators_list)))
 
-for p in permutation:
-    t = 0
-    n = arr[0]
-    
-    for i in range(1, N):
-        if p[i-1] == "//":
-            if n < 0:
-                n = -(-n // arr[i])
-            else:
-                n //= arr[i]
+
+def calculate(num1, num2, op):
+    if op == 0:
+        return num1 + num2
+    elif op == 1:
+        return num1 - num2
+    elif op == 2:
+        return num1 * num2
+    else:
+        if num1 < 0:
+            return -(-num1 // num2)
         else:
-            n = eval(str(n) + p[i-1] + str(arr[i]))
+            return num1 // num2
 
-    if n < min:
-        min = n
-    
-    if n > max:
-        max = n
 
-print(max)
-print(min)
+min_answer = float('inf')
+max_answer = -float('inf')
+
+for perm_operators in permutations_list:
+    temp_answer = numbers[0]
+    for i in range(1, len(numbers)):
+        temp_answer = calculate(temp_answer, numbers[i], perm_operators[i - 1])
+    min_answer = min(min_answer, temp_answer)
+    max_answer = max(max_answer, temp_answer)
+
+print(max_answer)
+print(min_answer)
