faae95391268462286ef84e4691038893bfe85c1
한주형 <wngud4950@gmail.com>
2025-02-12 21:37:51 +0900

    [Silver I] Title: 전쟁 - 전투, Time: 140 ms, Memory: 115060 KB -BaekjoonHub

--- 백준/Silver/1303. 전쟁 － 전투/전쟁 － 전투.py
+++ 백준/Silver/1303. 전쟁 － 전투/전쟁 － 전투.py
@@ -1,39 +1,49 @@
 from collections import deque
-
 m, n = map(int, input().split())
-graph = [list(input().rstrip()) for _ in range(n)]
-visited = [[False] * m for _ in range(n)]
-result = [0, 0]
+maps = [list(input()) for _ in range(n)]
 
-dx = [0, 0, -1, 1]
+w_answer = 0
+b_answer = 0
+
+visited = [[False] * m for _ in range(n)]
+
+dx = [0, 0, 1, -1]
 dy = [1, -1, 0, 0]
+
+def bfs(x, y):
+    queue = deque()
+    queue.append([x, y])
+    count = 0
     
-def bfs(i, j):
-    q = deque()
-    q.append((i, j))
-    visited[i][j] = True
-    cnt = 1
+    a = maps[x][y]
     
-    while q:
-        x, y = q.popleft()
+    while len(queue):
+        sx, sy = queue.popleft()
+        
+        count += 1
         
         for k in range(4):
-            nx = x + dx[k]
-            ny = y + dy[k]
+            nx = dx[k] + sx
+            ny = dy[k] + sy
             
             if 0 <= nx < n and 0 <= ny < m:
-                if not visited[nx][ny] and graph[nx][ny] == graph[i][j]:
-                    cnt += 1
-                    q.append((nx, ny))
+                if not visited[nx][ny] and maps[nx][ny] == a:
                     visited[nx][ny] = True
-    return cnt ** 2
-
+                    queue.append([nx, ny])
+            
+    return count ** 2
+        
+    
+    
 for i in range(n):
     for j in range(m):
         if not visited[i][j]:
-            if graph[i][j] == 'W':
-                result[0] += bfs(i, j)
+            visited[i][j] = True
+            score = bfs(i, j)
+            
+            if maps[i][j] == "W":
+                w_answer += score
             else:
-                result[1] += bfs(i, j)
+                b_answer += score
 
-print(*result)
+print(w_answer, b_answer)
